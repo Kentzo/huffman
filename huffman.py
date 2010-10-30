cc4eb3cc294165f9224b2b7f30d3cdce202fde80
linux <dlyfar@dlyfar-desktop.(none)>
2010-10-30 21:31:50 +0700

    last rev

--- huffman.py
+++ huffman.py
@@ -7,6 +7,7 @@ __author__ = 'Kulakov Ilya'
 class Huffman:
     def __init__(self):
         self.table = {}
+        self.rev_table = {}
         self.block_size = 1
         self.offsets = []
 
@@ -36,31 +37,66 @@ class Huffman:
     def encode(self, string):
         self.buildTable(string)
         result = ''.join([self.table[i] for i in string])
-        rev_table = {}
+        self.rev_table = {}
         for key, value in self.table.items():
-            rev_table.update({value : key})
+            self.rev_table.update({value : key})
         codeword = ''
         offset = counter = 0
         for i in result:
             codeword += i
             counter += 1     
             offset += 1
-            if rev_table.has_key(codeword) and counter <= self.block_size * 8:
+            if self.rev_table.has_key(codeword) and counter <= self.block_size * 8:
                 codeword = ''
                 offset = 0
                 if counter == self.block_size * 8:
                     self.offsets.append(0)
                     counter = 0
-            elif not rev_table.has_key(codeword) and counter == self.block_size * 8:
+            elif not self.rev_table.has_key(codeword) and counter == self.block_size * 8:
                 self.offsets.append(offset)
                 offset = 0
                 counter = 0
         return result
 
+    def decodeBlock(self, string, block, i):
+        offset = 0
+        if i != 0:
+            offset = self.offsets[i-1]
+        start = i * self.block_size * 8 - offset
+        end = i * self.block_size * 8
+        block = string[start:end] + block
+        result = codeword = ''
+        for j in block:
+            codeword += j
+            if self.rev_table.has_key(codeword):
+                result += self.rev_table[codeword]
+                codeword = ''
+        return result               
+            
+
+    def decode(self, string):        
+        result = ''
+        for i in range(0, len(string), self.block_size * 8):
+            result += self.decodeBlock(string, string[i:i + self.block_size*8], i / (self.block_size * 8))
+        return result
+
 h = Huffman()
-print h.encode('addddddddddddddd33333333333333444444444444fffffffffffvvvvvvvvbc')
+text_to_encode = open('./test2').read()
+print len(text_to_encode)
+encoded = h.encode(text_to_encode)
+keys = h.rev_table.keys()
+keys.sort()
+print keys
 print h.table
+for key in keys:
+    print key
 print h.offsets
+print encoded
+decoded = h.decode(encoded)
+print decoded
+print text_to_encode == decoded 
+
+
             
 
 
